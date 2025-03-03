3e902740cd5638d7f2a160004a22ac413810cec6
dung23423 <dungtrieu834@gmail.com>
2025-03-03 17:03:33 +0700

    [add] lab-01 ex04 and .gitignore

---
+++ lab-01/ex04/SinhVien.py
@@ -0,0 +1,8 @@
+class SinhVien:
+    def __init__(self, id, ten, gioi_tinh, chuyen_nganh, dtb):
+        self._id = id 
+        self._ten = ten
+        self._gioi_tinh = gioi_tinh
+        self._chuyen_nganh = chuyen_nganh
+        self._dtb = dtb
+        self._hoc_luc = ""
