5d524e2b1e91e48a081f436c6dcaf65a6ad6c85e
dung23423 <147330372+dung23423@users.noreply.github.com>
2025-03-13 20:08:14 -0700

    Add files via upload

---
+++ New folder/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using TrieuXuanDung_22800600424_B3.Models;
+
+namespace TrieuXuanDung_22800600424_B3.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
